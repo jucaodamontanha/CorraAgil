804af24e2238a7edb0b44b3e243037527532f74b
lineker xavier <jucaodamontanha@gmail.com>
2024-11-24 17:59:37 -0300

    enviado projeto

--- src/main/java/br/com/pipocaagil/corraagil/Cadastro/CadastroNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.pipocaagil.corraagil.Cadastro;
-
-public class CadastroNotFoundException extends RuntimeException {
-    public CadastroNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/pipocaagil/corraagil/Cadastro/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pipocaagil.corraagil.Cadastro;
-
-    import org.springframework.data.jpa.repository.JpaRepository;
-    import org.springframework.stereotype.Repository;
-
-@Repository
-    public interface CadastroRepository  extends JpaRepository< CadastroModel, Long> {
-        CadastroModel findByEmail(String email);
-    }
