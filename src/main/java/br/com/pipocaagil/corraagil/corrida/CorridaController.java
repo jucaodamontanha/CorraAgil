122f9cc54466e0bf2107f73cbe9bfca6ee06a099
jucaodamontanha <jucaodamontanha@gmail.com>
2025-03-27 01:58:21 -0300

    alterado config bd

--- src/main/java/br/com/pipocaagil/corraagil/corrida/CorridaController.java
+++ src/main/java/br/com/pipocaagil/corraagil/corrida/CorridaController.java
@@ -1,10 +1,8 @@
 package br.com.pipocaagil.corraagil.corrida;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
 import java.util.List;
 
 /**
