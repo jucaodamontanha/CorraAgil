81e9fd923bdce26c0faad17f3adef74e5433c919
jucaodamontanha <jucaodamontanha@gmail.com>
2025-07-11 19:42:34 -0300

    alterado envio de email

--- src/main/java/br/com/pipocaagil/corraagil/Cadastro/EmailService.java
+++ src/main/java/br/com/pipocaagil/corraagil/Cadastro/EmailService.java
@@ -1,14 +1,11 @@
 package br.com.pipocaagil.corraagil.Cadastro;
 
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
 
 /**
  * Serviço para envio de emails de confirmação com suporte a HTML.
@@ -19,13 +16,6 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    /**
-     * Envia um email de confirmação com corpo HTML.
-     *
-     * @param to          destinatário do email
-     * @param subject     assunto do email
-     * @param htmlContent conteúdo do email em HTML
-     */
     public void sendConfirmationEmail(String to, String subject, String htmlContent) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
@@ -33,18 +23,15 @@ public class EmailService {
 
             helper.setTo(to);
             helper.setSubject(subject);
-
-            // Usa o HTML com uma referência cid
             helper.setText(htmlContent, true);
 
-            // Adiciona imagem inline
-            FileSystemResource image = new FileSystemResource(new File("src/main/resources/imagem.png"));
+            ClassPathResource image = new ClassPathResource("imagem.png");
             helper.addInline("logoCorraAgil", image);
 
             mailSender.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException("Erro ao enviar e-mail de confirmação", e);
+        } catch (Exception e) {
+            System.err.println("Falha ao enviar e-mail de confirmação: " + e.getMessage());
+            //opcional: log.warn("Erro ao enviar email", e);
         }
     }
-
 }
